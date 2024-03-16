8de6099e052ac2e8e60b94c3d86fa33b27e1eb15
trakt <trakt@DESKTOP-F1DCND3>
2024-03-16 19:03:11 +0200

    Добавьте файлы проекта.

---
+++ 19.03.2024/19.03.2024.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int length(int n) 
+{
+    int l = 0;
+    do 
+    {
+       l++;
+       n /= 10;
+    } while (n);
+    return l;
+}
+
+void luckyticket(int n)
+{
+    int a = 0, b = 0, c = 0, d = 0, e = 0, f = 0;
+    a = n % 10;
+    b = (n / 10) % 10;
+    c = (n / 100) % 10;
+    d = (n / 1000) % 10;
+    e = (n / 10000) % 10;
+    f = (n / 100000) % 10;
+    if (a + b + c == e + d + f)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+}
+
+void ticket(int n)
+{
+    if (length(n) <= 5)
+    {
+       throw "Length of number <= 5";
+    }
+    else if (length(n) >= 7)
+    {
+        throw exception("Length of number >= 7");
+    }
+    luckyticket(n);
+}
+
+int main()
+{
+    int number;
+    cout << "Enter numbers: ";
+    cin >> number;
+    try
+    {
+        ticket(number);
+    }
+    catch (const char* p)
+    {
+        cout << p << endl;
+    }
+    catch (const exception& ex)
+    {
+        cout << ex.what() << endl;
+    }
+
+}
